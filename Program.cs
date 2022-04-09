fed07211d9cc9f543c7df3d26f5d3f4234afd3e6
Сергей <seregabur@live.ru>
2022-04-09 11:02:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Massiv5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minimumValue = 0;
+            int maximumValue = 30;
+            int maximumArrayLength = 30;
+
+            int[] array = new int[maximumArrayLength];
+            int lastElement = array.Length - 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minimumValue, maximumValue);
+            }
+
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.Write("\n");
+
+            int numberRepeat = 0;
+            int numberRepeatCount = 1;
+            bool isRepeatNumbers = false;
+            bool isRepeatTask = false;
+            int startingPosition = 1;
+
+            int numberRepeatTemp = 0;
+            int numberRepeatCountTemp = startingPosition;
+
+
+            for (int i = 0; i < array.Length-1; i++)
+            {
+
+                if(array[i]==array[i+1])
+                {
+                    numberRepeatTemp = array[i];
+                    numberRepeatCountTemp++;
+                    isRepeatNumbers = true;
+                    isRepeatTask = true;
+                }
+                else
+                {
+                    if(isRepeatNumbers==true)
+                    {
+                       if(numberRepeatCountTemp> numberRepeatCount)
+                        {
+                            numberRepeat = array[i];
+                            numberRepeatCount = numberRepeatCountTemp;
+                        }
+
+                      numberRepeatCountTemp = startingPosition;
+                      isRepeatNumbers = false;
+                    }
+                }
+            }
+
+            if(isRepeatTask==true)
+            {
+                Console.Write("Число с наибольшей серией повторений = " + numberRepeat + " повторений в серии - " + numberRepeatCount);
+            }
+            else
+            {
+                Console.Write("Повторений в данном масиве нет");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
